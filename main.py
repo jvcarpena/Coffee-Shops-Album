58394819baa859233c07922cc95081a0eafbccc1
jvcarpena <jvcarpena2@gmail.com>
2025-01-07 07:56:12 +0800

    Updated the imports

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
